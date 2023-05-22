54150b00159b551da10cc570b2093ba56a29e7ce
jonioak <101118421+jonioak@users.noreply.github.com>
2023-05-22 17:29:14 +0200

    random test added

---
+++ src/main/Tests/com/example/watsplanning3/com/example/watsplanning3/RandomPlannerTest.java
@@ -0,0 +1,18 @@
+package com.example.watsplanning3;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RandomPlannerTest {
+
+    @Test
+    void createDag() {
+        // arrange
+
+        // act
+
+        // assert
+
+    }
+}
